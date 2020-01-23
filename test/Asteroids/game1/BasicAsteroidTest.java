50bbabea5bb58520e07cfe882e0e5388720e40cb
Michael Allport <ma18533@essex.ac.uk>
2020-01-23 16:33:10 +0000

    added test for asteroids positional change

--- test/Asteroids/game1/BasicAsteroidTest.java
+++ test/Asteroids/game1/BasicAsteroidTest.java
@@ -4,25 +4,42 @@ import Asteroids.utilities.JEasyFrame;
 import org.junit.Before;
 import org.junit.Test;
 
-import static Asteroids.game1.Constants.DELAY;
+import static Asteroids.game1.Constants.*;
 import static org.junit.Assert.*;
 
 public class BasicAsteroidTest {
     public static BasicGame game;
+
     @Before
     public void BasicAsteroidTest()
     {
-        BasicGame game = new BasicGame();
+        game = new BasicGame();
     }
 
     @Test
-    public void asteroidMove1() throws Exception
+    public void asteroidMovex() throws Exception
     {
         BasicAsteroid a = game.asteroids.get(0);
         double x = a.getX();
         double y = a.getY();
         a.getY();
         Thread.sleep(DELAY);
+        double newx = ((x + (a.getVx() * DT)) + FRAME_WIDTH) % FRAME_WIDTH;
+        double newy = ((y * DT) + FRAME_HEIGHT) % FRAME_HEIGHT;
+        x = a.getX();
+        y = a.getY();
+        assertEquals(x, newx, 2);
+    }
 
+    @Test
+    public void asteroidMovey() throws Exception
+    {
+        BasicAsteroid a = game.asteroids.get(0);
+        double y = a.getY();
+        a.getY();
+        Thread.sleep(DELAY);
+        double newy = ((y + (a.getVx() * DT)) + FRAME_HEIGHT) % FRAME_HEIGHT;
+        y = a.getY();
+        assertEquals(y, newy, 2);
     }
 }
